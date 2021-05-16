cdd6df016e4df70cc85b2b6ecb20e857bd716073
Michael D Henderson <mdhender@mdhender.com>
2021-05-16 21:39:24 +0000

    add logger for testing rnd

---
+++ src/testrnd.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+unsigned long last_random = 1924085713L;
+
+int rnd(unsigned int max) {
+    unsigned long a, b, c, cong_result, shift_result;
+
+    /* For congruential method, multiply previous value by the prime number 16417. */
+    a           = last_random;
+    b           = last_random << 5;
+    c           = last_random << 14;
+    cong_result = a + b + c;            /* Effectively multiply by 16417. */
+
+    /* For shift-register method, use shift-right 15 and shift-left 17 with no-carry addition (i.e., exclusive-or). */
+    a             = last_random >> 15;
+    shift_result  = a ^ last_random;
+    a             = shift_result << 17;
+    shift_result ^= a;
+
+    last_random = cong_result ^ shift_result;
+
+    a = last_random & 0x0000FFFF;
+
+    return((int)((a * (long)max) >> 16) + 1L);
+}
+
+int main() {
+    int i, r;
+    for (i = 0; i < 1000000; i++) {
+        r = rnd(1024*1024);
+        if (i < 10) {
+            printf("%9d %9d\n", i, r);
+        } else if (1000 < i && i < 1010) {
+            printf("%9d %9d\n", i, r);
+        } else if ((i % 85713) == 0) {
+            printf("%9d %9d\n", i, r);
+        }
+    }
+}
+
